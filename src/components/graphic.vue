dd3ab5ce52d26506d90abfa6a3bc05ff53e4663e
zhaoyutong <yitongzhao@163.com>
2018-07-27 21:05:15 +0800

    first add one-vue

---
+++ src/components/graphic.vue
@@ -0,0 +1,114 @@
+<template>
+    <div id="graphic">
+        <img class="img" :src="graphic.img_url" alt="">
+        <span class="title_pic_info">{{ graphic.title + ' | ' + graphic.pic_info }}</span>
+        <p class="forward">{{ graphic.forward }}</p>
+        <span class="words_info">{{ graphic.words_info }}</span>
+        <div class="label">
+            <div class="find">
+                <i class="find_logo"></i>
+                发现
+            </div>
+            <div class="operation">
+                <span class="note"></span>
+                <span class="collection"></span>
+                <span class="repost"></span>
+                <span class="like">
+                    <span class="like_logo"></span>
+                    <span class="like_count">{{ graphic.like_count }}</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['graphic'],
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    #graphic{
+        text-align: center;
+        background-color: #fff;
+
+        .img{
+            width: 100%;
+        }
+        .title_pic_info, .words_info{
+            font-size: 12px;
+            color: #808080;
+        }
+        .forward{
+            margin: 40px 30px 30px;
+            text-align: left;
+            font-size: 13px;
+        }
+        .label{
+            height: 20px;
+            margin-top: 40px;
+
+            .find{
+                float: left;
+                margin-left: 20px;
+                font-size: 10px;
+                .find_logo{
+                    display: inline-block;
+                    height: 15px;
+                    width: 15px;
+                    background-image: url('../assets/logo.png');
+                    background-size: cover;
+                    vertical-align: middle;
+                }
+            }
+            .operation{
+                float: right;
+                >span{
+                    display: inline-block;
+                    height: 15px;
+                    width: 15px;
+                    margin-right: 25px;
+                    background-size: cover;
+                    &.note{
+                        background-image: url('../assets/logo.png');
+                    }
+                    &.collection{
+                        background-image: url('../assets/logo.png');
+                    }
+                    &.repost{
+                        background-image: url('../assets/logo.png');
+                    }
+                    &.like{
+                        width: auto;
+                        .like_logo{
+                            display: inline-block;
+                            height: 15px;
+                            width: 15px;
+                            background-image: url('../assets/logo.png');
+                            background-size: cover;
+                            &.active{
+                                background-image: url('../assets/logo.png');
+                            }
+                        }
+                        .like_count{
+                            position: relative;
+                            top: -10px;
+                            font-size: 10px;
+                            color: #a6a6a6;
+                        }
+                    }
+                    &:last-child{
+                        margin-right: 5px;
+                    }
+                }
+            }
+        }
+    }
+</style>
+
---
+++ src/components/overview.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="overview">
+        <div class="volume" :class="{ unfold : unfold }" @click="toggle">
+            一个 {{ lists[0].volume }}
+            <i></i>
+        </div>
+        <ul class="list" :class="{ unfold : unfold }">
+            <li v-for="list in lists" :key="list.id">
+                <i></i>
+                <div class="item">
+                    <p class="category">ONE STORY</p>
+                    <p class="title">{{ list.title }}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['lists'],
+    data() {
+        return {
+            unfold: false
+        }
+    },
+    methods: {
+        toggle: function () {
+            this.unfold = !this.unfold;
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+#overview {
+    margin: 5px 0;
+    background-color: #fff;
+
+    .volume {
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        font-size: 12px;
+        color: #808080;
+        i {
+            display: inline-block;
+            width: 12px;
+            height: 12px;
+            margin-left: 3px;
+            background-image: url(../assets/logo.png);
+            background-size: cover;
+            vertical-align: middle;
+            transition: transform .5s;
+        }
+        &.unfold {
+            i {
+                transform: rotateZ(180deg);
+            }
+        }
+    }
+
+    .list {
+        display: none;
+        &.unfold {
+            display: block;
+        }
+        li {
+            position: relative;
+            margin: 20px 30px 0 20px;
+            i {
+                display: inline-block;
+                height: 12px;
+                width: 12px;
+                margin-right: 5px;
+                background-image: url(../assets/logo.png);
+                background-size: cover;
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+            }
+            .item {
+                display: inline-block;
+                margin-left: 20px;
+                .category {
+                    font-size: 12px;
+                }
+                .title {
+                    font-size: 15px;
+                }
+            }
+        }
+    }
+}
+</style>
