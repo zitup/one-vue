27f721b95e8d3938a95be865d655ea7cf44218fa
zhaoyutong <yitongzhao@163.com>
2018-07-28 19:46:11 +0800

    two add one-vue

--- src/components/overview.vue
+++ src/components/overview.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="overview">
         <div class="volume" :class="{ unfold : unfold }" @click="toggle">
-            一个 {{ lists[0].volume }}
+            一个 VOL.{{ menu.vol }}
             <i></i>
         </div>
         <ul class="list" :class="{ unfold : unfold }">
-            <li v-for="list in lists" :key="list.id">
+            <li v-for="list in menu.list" :key="list.content_id">
                 <i></i>
                 <div class="item">
-                    <p class="category">ONE STORY</p>
+                    <p class="category">{{ list.tag && list.tag.title || category[list.content_type-1] }}</p>
                     <p class="title">{{ list.title }}</p>
                 </div>
             </li>
@@ -18,10 +18,11 @@
 
 <script>
 export default {
-    props: ['lists'],
+    props: ['menu'],
     data() {
         return {
-            unfold: false
+            unfold: false,
+            category: ['ONE STORY', '连载', '问答', '音乐', '影视']
         }
     },
     methods: {
@@ -62,6 +63,7 @@ export default {
 
     .list {
         display: none;
+        padding-bottom: 20px;
         &.unfold {
             display: block;
         }
@@ -81,12 +83,15 @@ export default {
             }
             .item {
                 display: inline-block;
-                margin-left: 20px;
+                margin-left: 24px;
                 .category {
-                    font-size: 12px;
+                    font-size: 10px;
+                    line-height: 10px;
                 }
                 .title {
-                    font-size: 15px;
+                    font-size: 13px;
+                    line-height: 13px;
+                    margin-top: 6px;
                 }
             }
         }
