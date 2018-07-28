11f4ec16a2f6eda1eeaa9e7263354e9b4ff04cec
zhaoyutong <zhaoyutong@iautos.cn>
2018-07-28 19:46:11 +0800

    two add one-vue

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,18 +1,29 @@
 <template>
-    <div id="card">
-        <p class="category">- 连载 -</p>
-        <p class="title">风从海上里6</p>
-        <p class="author">文/姚瑶</p>
-        <img src="../assets/" alt="">
-        <p class="forward"></p>
-        <div>
-            <span class="date"></span>
-            <div>
-                <span class="like">
-                    <span class="like_logo"></span>
-                    <span class="like_count">{{ graphic.like_count }}</span>
-                </span>
-                <span class="repost"></span>
+    <div>
+        <div id="card" v-for="card in cards" :key="card.item_id">
+            <p class="category">- {{ card.tag_list[0] && card.tag_list[0].title || category[card.category-1] }} -</p>
+            <p class="title">{{ card.title }}</p>
+            <p class="author">文 / {{ card.author.user_name }}</p>
+            <div class="music" v-if="card.category == 4">
+                <div>
+                    <span></span>
+                    <img :src="card.img_url" class="img">
+                    <span></span>
+                    <span>STORIRS OF MUSIC</span>
+                </div>
+                <p>{{ music_title }}</p>
+            </div>
+            <img :src="card.img_url" class="img" v-else>
+            <p class="forward">{{ card.forward }}</p>
+            <div class="label">
+                <span class="date">{{ getDate(card.post_date) }}</span>
+                <div class="operation">
+                    <span class="like">
+                        <span class="like_logo"></span>
+                        <span class="like_count">{{ card.like_count }}</span>
+                    </span>
+                    <span class="repost"></span>
+                </div>
             </div>
         </div>
     </div>
@@ -20,32 +31,101 @@
 
 <script>
 export default {
-    
+    props: ['cards'],
+    data() {
+        return {
+            category: ['ONE STORY', '连载', '问答', '音乐', '影视'],
+            music_title: ''
+        }
+    },
+    methods: {
+        getDate: function (post_date) {
+            let today = new Date().getDate();
+            if (post_date.indexOf(today) > -1) {
+                return '今天'
+            } else {
+                let mouth = post_date.substr(5, 2);
+                let day = post_date.substr(8, 2);
+                return mouth + '月' + day + '日'
+            }
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
-    #card{
-        padding: 10px 20px;
-        margin-bottom: 20px;
-        background-color: #fff;
+@import "../style/mixin";
+#card {
+    padding: 10px 20px;
+    margin-bottom: 20px;
+    background-color: #fff;
 
-        .category{
-            text-align: center;
-            font-size: 10px;
-            color: #808080;
-        }
-        .title{
-            font-size: 16px;
-        }
-        .author{
-            font-size: 12px;
-        }
-        img{
-            width: 100%;
-        }
-        .forward{
-            
+    .category {
+        text-align: center;
+        font-size: 10px;
+        color: #808080;
+    }
+    .title {
+        margin: 10px 0;
+        font-size: 16px;
+    }
+    .author {
+        font-size: 12px;
+        color: #808080;
+    }
+    .img {
+        margin: 10px 0;
+        width: 100%;
+    }
+    .music {
+        .img {
+            display: block;
+            width: 60%;
+            margin: 10px auto;
+            border-radius: 100%;
         }
     }
+    .forward {
+        color: #808080;
+        font-size: 13px;
+    }
+    .label {
+        height: 20px;
+        line-height: 20px;
+        margin-top: 20px;
+        color: #808080;
+        .date {
+            float: left;
+            font-size: 10px;
+            color: #aaaaaa;
+        }
+        .operation {
+            float: right;
+            .like {
+                margin-right: 20px;
+                .like_logo {
+                    display: inline-block;
+                    width: 15px;
+                    height: 15px;
+                    .bgi("../assets/logo.png");
+                }
+                .like_count {
+                    position: relative;
+                    top: -10px;
+                    font-size: 10px;
+                    color: #a6a6a6;
+                }
+            }
+            .repost {
+                display: inline-block;
+                width: 15px;
+                height: 15px;
+                .bgi("../assets/logo.png");
+            }
+        }
+        &:after {
+            .clear();
+        }
+    }
+}
 </style>
--- src/page/home.vue
+++ src/page/home.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+    <div v-if="data">
         <headTop :weather="data.weather" v-if="data.weather" />
-        <graphic :graphic="data.content_list[0]" v-if="data.content_list" />
-        <overview :lists="data.content_list" v-if="data.content_list"/>
+        <graphic :graphic="graphic" v-if="graphic" />
+        <overview :menu="menu" v-if="menu"/>
+        <card :cards="cards" v-if="cards"/>
     </div>
 </template>
 
@@ -10,20 +11,25 @@
 import headTop from '../components/header';
 import graphic from '../components/graphic';
 import overview from '../components/overview';
+import card from '../components/card';
 
 export default {
     data() {
         return {
-            data: []
+            data: [],
+            graphic: {},
+            menu: {},
+            cards: []
         }
     },
     components:{
         headTop,
         graphic,
-        overview
+        overview,
+        card
     },
     created() {
-        fetch('http://v3.wufazhuce.com:8000/api/onelist/4845/0?channel=wdj&version=4.0.2&uuid=ffffffff-a90e-706a-63f7-ccf973aae5ee&platform=android', {
+        fetch('http://v3.wufazhuce.com:8000/api/onelist/4848/0?channel=wdj&version=4.0.2&uuid=ffffffff-a90e-706a-63f7-ccf973aae5ee&platform=android', {
             method: 'GET'
         })
             .then((res) => {
@@ -31,6 +37,9 @@ export default {
             })
             .then((res) => {
                 this.data = res.data;
+                this.graphic = res.data.content_list[0];
+                this.menu = res.data.menu;
+                this.cards = res.data.content_list.slice(1);
             })
     }
 }
